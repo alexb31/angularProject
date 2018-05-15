f3d22e18217f16b0b6d4b1dbbc3a57f53f3f7eb5
Unknown <marinealexandre5@gmail.com>
2018-05-15 14:39:31 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/app/Routers/app-routing/app-routing.module.ts
+++ src/app/Routers/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {AppComponent} from '../../app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../../app.component';
 import { CompanyInfoComponent } from '../../components/company-info/company-info.component';
 
 const appRoutes: Routes = [
