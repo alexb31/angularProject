ba7025a95d398f1af89f1077479565cc4951d4e2
alexb31 <alexandrebornstein@gmail.com>
2018-05-15 11:06:38 +0200

    Added services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpacexApiService } from './BackEnd/spacex-api.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private spacexService: SpacexApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.spacexService.getCompanyInfo()
+                      .subscribe(data =>
+                      console.log(JSON.stringify(data)));
+  }
 }
