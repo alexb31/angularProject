db24f9e3d58c0ffd9bdf57205b217eb3dfe313b4
Unknown <alexandrebornstein@gmail.com>
2018-05-17 11:50:31 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/components/launches-info/launches-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-launches-info',
+  templateUrl: './launches-info.component.html',
+  styleUrls: ['./launches-info.component.css']
+})
+export class LaunchesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/rockets-info/rockets-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rockets-info',
+  templateUrl: './rockets-info.component.html',
+  styleUrls: ['./rockets-info.component.css']
+})
+export class RocketsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
